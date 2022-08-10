19ba400b176b83c6b9b5ccc8dc3657d4d59600aa
Алексеев Алексей Александрович <a.alekseev@avilex.ru>
2022-08-10 10:51:03 +0700

    *

--- cypress.config.js
+++ cypress.config.js
@@ -15,6 +15,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "https://askona:askona@beta.askona.ru",
+    baseUrl: "https://askona.ru",
   },
 });
--- cypress/e2e/pages/MainPage.js
+++ cypress/e2e/pages/MainPage.js
@@ -1,13 +1,25 @@
 class MainPage {
     search = '#search-form-input'
     submit = '#search-form button.header-main__submit'
+    productCard = 'a[data-const=product_link'
 
     open(){
         cy.visit('/')
     }
 
-   
+    openMattressPage(){
+        cy.visit('/matrasy')
+    }
 
+    openRandomCard(){
+        cy.get(this.productCard).eq(3).click()
+    }
+
+    getCount(){
+        return cy.get(this.productCard).then(($el) => {
+            const itemCount = $el.length;
+        })
+    }
 }
 
 export default MainPage
