a61f3b6c7e82074b19674b95fc1a661ad9439133
Алексеев Алексей Александрович <a.alekseev@avilex.ru>
2022-08-11 16:02:11 +0700

    *

--- cypress/e2e/pages/ProductPage.js
+++ cypress/e2e/pages/ProductPage.js
@@ -5,6 +5,8 @@ class ProductPage {
     baseItem = 'a.modal-size__item'
     addToBasketElement = 'button#product-info__btn-buy'
     popupAddToCart = 'div#added-to-cart'
+    mattressPrice = 'p.product-card-2022-meta__price-new'
+    popupPrice = 'span.price_new'
 
     selectBase(){
         click(this.base)
@@ -15,6 +17,20 @@ class ProductPage {
         click(this.addToBasketElement)
         waitVisibilityElement(this.popupAddToCart)
     }
+
+    getPriceObject() {
+        return cy.get(this.mattressPrice).then((price) => {
+            return price.text()
+        });
+    }
+
+    getTextElement(locator){
+        return cy.get(locator).then((element) => {
+            return element.text()
+        })
+    }
+
+
 
 
 
--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -1,15 +1,29 @@
 import MainPage from "./pages/MainPage"
 import ProductPage from "./pages/ProductPage";
-require("cypress-xpath");
-describe('empty spec', () => {   
-  const main = new MainPage();
-  const product = new ProductPage();
-  
-  it("Открытие страницы матрасов", () => {
-    main.openMattressPage()
-    main.openRandomCard()
-    product.selectBase()
-    product.addToBasket()
-  });
 
+require("cypress-xpath");
+describe('empty spec', () => {
+    const main = new MainPage();
+    const product = new ProductPage();
+    let priceBefore;
+    it("Открытие страницы матрасов", () => {
+        main.openMattressPage()
+        main.openRandomCard()
+
+        cy.get(product.mattressPrice).invoke("text").then((text) => {
+            priceBefore = text;
+            return text;
+        })
+
+    });
+
+    it("test variable", () => {
+        cy.log(priceBefore)
+        product.selectBase()
+        product.addToBasket()
+        cy.get(product.popupPrice).then((text)=>{
+            cy.log("price1 - " + priceBefore + "        price2 - " + text.text())
+            expect(priceBefore).equal(text.text())
+        })
+    })
 });
